a021dae00350af112721b50f0f1131c917b0ea6d
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-01 01:14:08 -0300

    primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  /* if (ids.length === 0) {
+    return []
+  }
+  return ids.map((id) => data.species.find((specie) => (specie.id --- id))); */
+  const getSpecies = species.filter((specie) => ids.find((id) => id === specie.id));
+  return getSpecies;
 }
 
 module.exports = getSpeciesByIds;
