ae1f2e1cedb41fa6ecd8db8f34e5fa0d10a08755
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-04 14:58:00 -0300

    realizado requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const [employ] = employees.filter((employee) => employee.id === id);
+  const firstAnimal = employ.responsibleFor[0];
+  const [animal] = species.filter((specie) => specie.id === firstAnimal);
+  const oldest = animal.residents.sort((y, z) => z.age - y.age)[0];
+  const result = [oldest.name, oldest.sex, oldest.age];
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
