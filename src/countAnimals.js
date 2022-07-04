9a290e728807edd18c5aa51d966c4ed54b04e40b
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-04 01:09:06 -0300

    quinto requisito realizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,19 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const animals = {};
+  if (!animal) {
+    data.species.forEach((element) => {
+      animals[element.name] = element.residents.length;
+    });
+    return animals;
+  }
+
+  if (!animal.sex) {
+    return data.species.find((element) => element.name === animal.specie).residents.length;
+  }
+  return data.species.find((elment) => elment.name === animal.specie).residents.filter((elment) =>
+    elment.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
