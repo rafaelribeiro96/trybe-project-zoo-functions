bd52523b0bfca08595f5d1bf9c12306dbd691ef3
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-04 00:10:05 -0300

    quarto requisito realizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,21 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
-}
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 
+  const employees = [''];
+  data.employees.forEach((employ) => {
+    if (employ.managers.includes(managerId)) {
+      employees.push(`${employ.firstName} ${employ.lastName}`);
+    }
+  });
+  return employees;
+}
 module.exports = { isManager, getRelatedEmployees };
