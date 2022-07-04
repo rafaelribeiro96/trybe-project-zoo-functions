5cfb65d2f663a94e32ea003324855e67a9fd4c22
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-07-03 23:07:02 -0300

    segundo requisito realizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animals = data.species.find((element) => element.name === animal).residents;
+  return animals.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
